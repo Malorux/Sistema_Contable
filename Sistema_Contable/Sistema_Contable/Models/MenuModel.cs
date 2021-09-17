fe54b7aae60e4341dda927f3b5564c39b697057e
Your Name <you@example.com>
2021-09-16 18:49:40 -0600

    Mauro Gómez

---
+++ Sistema_Contable/Sistema_Contable/Models/MenuModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Sistema_Contable.Models
+{
+    public class MenuModel
+    {
+        public ICommand M_Command { get; }
+
+        public MenuModel()
+        {
+            M_Command = new CommandViewModel(Execute);
+        }
+
+        public string MenuName { get; set; }
+        public PathGeometry IconData { get; set; }
+        public Page Pagina { get; set; }
+
+
+        private void Execute()
+        {
+            foreach (Window window in Application.Current.Windows)
+            {
+
+                if (window.GetType() == typeof(MainWindow))
+                {
+                    (window as MainWindow).MainWindowFrame.Navigate(Pagina);
+
+                }
+
+            }
+
+        }
+
+    }
+}
+
---
+++ Sistema_Contable/Sistema_Contable/ViewModels/MenuViewModels.cs
@@ -0,0 +1,42 @@
+﻿
+using Sistema_Contable.Models;
+using Sistema_Contable.View;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Sistema_Contable.ViewModels
+{
+    public class MenuViewModels
+    {
+
+        //i will drop the download link for assets in the description
+        private ResourceDictionary dictionary = Application.LoadComponent(new Uri("/Sistema_Contable;component/Assets/icons.xaml", UriKind.RelativeOrAbsolute)) as ResourceDictionary;
+
+        ObservableCollection<MenuModel> _menuItems { get; set; }
+
+        public ObservableCollection<MenuModel> MenuItems
+        {
+            get { return _menuItems; }
+            set { _menuItems = value; }
+        }
+
+
+        public MenuViewModels()
+        {
+            _menuItems = new ObservableCollection<MenuModel>()
+            {
+                new MenuModel(){ IconData=(PathGeometry)dictionary["dashboard"],MenuName="Contabilidad",Pagina = new PContabilidad()},
+                new MenuModel(){ IconData=(PathGeometry)dictionary["allcourses"],MenuName="Finanzas"},
+                new MenuModel(){ IconData=(PathGeometry)dictionary["folder"], MenuName="Compras" },
+                new MenuModel(){ IconData=(PathGeometry)dictionary["profile"], MenuName="Ventas" },
+                new MenuModel(){ IconData=(PathGeometry)dictionary["chats"], MenuName="Inventario" },
+                new MenuModel(){ IconData=(PathGeometry)dictionary["estrella"], MenuName="Reportes" },
+                new MenuModel(){ IconData=(PathGeometry)dictionary["settings"], MenuName="Configuracion" }
+            };
+        }
+    }
+}
