632a0fbe4d57495e5cf554d3272680c8d07fa824
Michaell Joel reyes <michaelljoel.reyes202@gmail.com>
2021-09-16 19:36:11 -0600

    Michaell Reyes

---
+++ Sistema_Contable/Sistema_Contable/Controller/PContabilidadControler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema_Contable.Controller
+{
+    class PContabilidadControler
+    {
+
+    }
+}
--- Sistema_Contable/Sistema_Contable/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ Sistema_Contable/Sistema_Contable/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -48,7 +48,7 @@ namespace Sistema_Contable {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -60,7 +60,7 @@ namespace Sistema_Contable {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Sistema_Contable;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Sistema_Contable;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -74,7 +74,7 @@ namespace Sistema_Contable {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.8.0")]
         public static void Main() {
             Sistema_Contable.App app = new Sistema_Contable.App();
             app.InitializeComponent();
