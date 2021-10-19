986eed87f6d4a1d4b028042129977f2de1f3eaba
Your Name <you@example.com>
2021-10-19 13:16:41 -0600

    Mauro Gómez

--- Sistema_Contable/Sistema_Contable/Models/AgregarCuenta.cs
+++ Sistema_Contable/Sistema_Contable/Models/AgregarCuenta.cs
@@ -1,14 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Sistema_Contable.Models
 {
     public class AgregarCuenta
     {
-        public int Id_Cuenta { get; set; }
-        public string Nombre_CuentaC { get; set; }
-        public decimal Debe { get; set; }
-        public decimal Haber { get; set; }
+        private int _Id_Cuenta;
+        private string _Nombre_CuentaC;
+        private decimal _Debe;
+        private decimal _Haber;
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var Handler = PropertyChanged;
+            Handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        public int Id_Cuenta
+        {
+            get => _Id_Cuenta;
+            set { _Id_Cuenta = value; RaisePropertyChanged("Id_Cuenta"); }
+        }
+
+        public string Nombre_CuentaC
+        {
+            get => _Nombre_CuentaC;
+            set { _Nombre_CuentaC = value; RaisePropertyChanged("Nombre_CuentaC"); }
+        }
+
+        public decimal Debe
+        {
+            get => _Debe;
+            set { _Debe = value; RaisePropertyChanged("Debe"); }
+        }
+
+        public decimal Haber
+        {
+            get => _Haber;
+            set { _Haber = value; RaisePropertyChanged("Haber"); }
+        }
     }
 }
